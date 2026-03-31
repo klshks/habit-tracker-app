f6cfe1b5891e27b52406820723bb8cc06dfec852
klshks <lyoshakyliushyk@gmail.com>
2026-03-31 19:07:34 +0300

    style: complete layout

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+let habits = [];
+const HABIT_KEY = 'HABIT_KEY';
+
+/* utils */
+
+function loadData() {
+    const habitsString = localStorage.getItem(HABIT_KEY);
+    const habitArray = JSON.parse(habitsString);
+    if (Array.isArray(habitArray)) {
+        habits = habitArray;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+/* render */
+
+function rerenderMenu (activeHabitId) {
+    if (!activeHabitId) {
+        return;
+    }
+
+    
+}
+
+/* init */
+
+(() => {
+    loadData();
+})();
+
